b37547c539a63121a827ffec61778239314f90fc
CristianCifuentes01 <124294094+CristianCifuentes01@users.noreply.github.com>
2023-09-30 14:27:36 -0500

    Add files via upload

--- prueba2/Persona.java
+++ prueba2/Persona.java
@@ -2,4 +2,37 @@ package pooClase;
 
 public class Persona {
 
+	private String nombre, email;
+	private int cedula;
+	
+	public Persona(){
+		 nombre="";
+		 email="";
+		 cedula=0;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	
 }
